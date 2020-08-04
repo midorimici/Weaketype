d58c734fa3b89576d34c575668a49dfcff228c30
midori mici <midorimici.works@gmail.com>
2020-08-04 23:09:29 +0900

    change assets path

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -356,7 +356,7 @@ module.exports = function(webpackEnv) {
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: 'static/weaketype/media/[name].[hash:8].[ext]',
               },
             },
             // Process application JS with Babel.
@@ -498,7 +498,7 @@ module.exports = function(webpackEnv) {
               // by webpacks internal loaders.
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               options: {
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: 'static/weaketype/media/[name].[hash:8].[ext]',
               },
             },
             // ** STOP ** Are you adding a new loader?
